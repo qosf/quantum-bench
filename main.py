109f0de7df1ff7f95f38bd9d2dc270434166ad5a
Tomas Babej <tomas@tbabej.com>
2018-07-24 22:55:24 -0400

    main: Add stub

---
+++ main.py
@@ -0,0 +1,36 @@
+import importlib
+from logger import LoggerMixin
+
+import collectors
+from plugins import Collector
+
+
+class EntryPoint(LoggerMixin):
+    """
+    The main class that governs the run of the data collection process.
+    """
+
+    def import_plugins(self):
+        for module in collectors.__all__:
+            try:
+                module_id = f"{collectors.__name__}.{module}"
+                importlib.import_module(module_id)
+                self.debug(f"{module_id} loaded successfully.")
+            except Exception as exc:
+                self.important(f"The module {module} could not be loaded: {exc}")
+
+    def load_configuration(self):
+        pass
+
+    def main(self):
+        self.import_plugins()
+        print(Collector.plugin_classes)
+        print(Collector.plugins)
+
+
+def main():
+    ep = EntryPoint()
+    ep.main()
+
+if __name__ == '__main__':
+    main()

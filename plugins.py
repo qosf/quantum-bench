edd7d264ef34dfb2ab6c946f2bd27da6025a6a03
Tomas Babej <tomas@tbabej.com>
2018-07-24 21:53:45 -0400

    plugins: Initial stub

---
+++ plugins.py
@@ -0,0 +1,48 @@
+from utils import classproperty
+
+
+class NoSuchPlugin(Exception):
+    """
+    Raised when a plugin could not be found.
+    """
+    pass
+
+
+class PluginMount(type):
+
+    def __init__(cls, name, bases, attrs):
+        super(PluginMount, cls).__init__(name, bases, attrs)
+
+        if not hasattr(cls, 'plugin_classes'):
+            cls.plugin_classes = []
+        else:
+            cls.plugin_classes.append(cls)
+
+
+class Plugin(metaclass=PluginMount):
+    """
+    Serves as a base class for all plugins.
+    """
+
+    @classproperty
+    def plugins(cls):
+        """
+        Returns a dictionary of Protocol plugins discovered.
+        """
+
+        return {
+            plugin_class.identifier: plugin_class
+            for plugin_class in cls.plugin_classes
+        }
+
+    @classmethod
+    def get(cls, identifier):
+        """
+        Returns a plugin class corresponding to the given identifier. Raises
+        NoSuchPlugin exception if none was found.
+        """
+
+        try:
+            return cls.plugins[identifier]
+        except KeyError:
+            raise NoSuchPlugin(f"Plugin '{identifier}' is not available")

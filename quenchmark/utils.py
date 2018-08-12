8f8d240d6048836f056f158bb0c3b08768954d59
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-08-12 00:13:28 -0400

    utils: Add run method

--- quenchmark/utils.py
+++ quenchmark/utils.py
@@ -2,6 +2,8 @@
 Implements utilities and helper functions.
 """
 
+import subprocess
+
 
 class classproperty(object):
     """
@@ -13,3 +15,15 @@ class classproperty(object):
 
     def __get__(self, obj, arg):
         return self.method(arg)
+
+
+def run(args, encoding='ascii'):
+    child = subprocess.Popen(
+        [str(arg) for arg in args],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = child.communicate()
+    code = child.returncode
+
+    return stdout.decode(encoding), stderr.decode(encoding), code

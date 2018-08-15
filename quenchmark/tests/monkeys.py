afa81838cc476b315d958b52480c43509951ca51
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-08-14 21:56:43 -0400

    more elegant lambda functions with property wrapper

--- quenchmark/tests/monkeys.py
+++ quenchmark/tests/monkeys.py
@@ -6,23 +6,28 @@ with unittest.mock in the tests.
 class MonkeyGithub():
     search_users = lambda self, x: [MonkeyUser()]
 
-class MonkeyUser():
-    get_repo = lambda self, y: MonkeyRepo()
-
 class MonkeyRepo():
     get_commits = lambda self: list(range(10))
-    @property
-    def created_at(self, *args):
-         return ValueError # raising error to ensure override in actual tests
+    created_at = property(lambda self, *args: 1/0) # raising error to ensure override in actual tests
     get_license = lambda self: MonkeyContentFile()
+    get_stats_contributors = lambda self: 1/0
+
+class MonkeyUser():
+    get_repo = lambda self, y: MonkeyRepo()
+    login = property(lambda self: 'Peter Shor')
+    weeks = property(lambda self: StatsContributor())
 
 class MonkeyContentFile():
-    @property
-    def license(self):
-        return MonkeyLicense()
+    license = property(lambda self: MonkeyLicense())
 
 class MonkeyLicense():
-    @property
-    def spdx_id(self):
-        return ValueError # raising error to ensure override in actual tests
+    spdx_id = property(lambda self: 1/0) # raising error to ensure override in actual tests
 
+class MonkeyStatsContributor():
+    author = property(lambda self: MonkeyUser())
+    weeks = property(lambda self: [MonkeyWeeks() for i in range(5)])
+
+class MonkeyWeeks(MonkeyStatsContributor):
+    a = property(lambda self: 100)
+    d = property(lambda self: 200)
+    c = property(lambda self: 50)

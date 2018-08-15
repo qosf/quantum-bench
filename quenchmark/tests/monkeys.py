294caeedd100679bf7852a0e1d10b58543a69b20
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-08-14 23:56:51 -0400

    tidied monkey classes and added new tests
    - tests for core_developers and get_total_adds_and_dels added

--- quenchmark/tests/monkeys.py
+++ quenchmark/tests/monkeys.py
@@ -8,9 +8,10 @@ class MonkeyGithub():
 
 class MonkeyRepo():
     get_commits = lambda self: list(range(10))
-    created_at = property(lambda self, *args: 1/0) # raising error to ensure override in actual tests
+    created_at = property(lambda self, *args: 1/0)
     get_license = lambda self: MonkeyContentFile()
     get_stats_contributors = lambda self: 1/0
+    get_stats_code_frequency = lambda self: 1/0
 
 class MonkeyUser():
     get_repo = lambda self, y: MonkeyRepo()
@@ -21,13 +22,17 @@ class MonkeyContentFile():
     license = property(lambda self: MonkeyLicense())
 
 class MonkeyLicense():
-    spdx_id = property(lambda self: 1/0) # raising error to ensure override in actual tests
+    spdx_id = property(lambda self: 1/0)
 
 class MonkeyStatsContributor():
     author = property(lambda self: MonkeyUser())
     weeks = property(lambda self: [MonkeyWeeks() for i in range(5)])
 
 class MonkeyWeeks(MonkeyStatsContributor):
-    a = property(lambda self: 100)
-    d = property(lambda self: 200)
-    c = property(lambda self: 50)
+    a = property(lambda self: 1/0)
+    d = property(lambda self: 1/0)
+    c = property(lambda self: 1/0)
+
+class MonkeyStatsCodeFrequency():
+    additions = property(lambda self: 1/0)
+    deletions = property(lambda self: 1/0)
--- quenchmark/tests/test_meta_collector.py
+++ quenchmark/tests/test_meta_collector.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 import pytest
 import github
+import numpy as np
 
 from quenchmark.collectors.meta import MetaCollector
 import monkeys
@@ -76,19 +77,78 @@ def test_has_xtrnl_issues_or_prs():
     """
     pass #TODO
 
+@patch('monkeys.MonkeyWeeks')
 @patch('monkeys.MonkeyStatsContributor')
 @patch('monkeys.MonkeyRepo')
 @patch('github.Github', mock.MagicMock(return_value=monkeys.MonkeyGithub()))
-def test_get_contributors(repo_mock, stats_mock):
+def test_get_contributors(repo_mock, stats_mock, weeks_mock):
     """
     Getting the list of contributors for a project.
     """
     repo_mock.return_value.get_stats_contributors.return_value = [monkeys.MonkeyStatsContributor() for i in range(5)]
     stats_mock.return_value.weeks = [monkeys.MonkeyWeeks() for i in range(5)]
     stats_mock.return_value.author= monkeys.MonkeyUser()
-    contributor_list = [{'additions': 500, 'commits': 250, 'deletions': 1000, 'name': 'Peter Shor'},
-            {'additions': 500, 'commits': 250, 'deletions': 1000, 'name': 'Peter Shor'},
-            {'additions': 500, 'commits': 250, 'deletions': 1000, 'name': 'Peter Shor'},
-            {'additions': 500, 'commits': 250, 'deletions': 1000, 'name': 'Peter Shor'},
-            {'additions': 500, 'commits': 250, 'deletions': 1000, 'name': 'Peter Shor'}]
+    weeks_mock.return_value.a = 100
+    weeks_mock.return_value.d = -100
+    weeks_mock.return_value.c = 20
+    contributor_list = [{'additions': 500, 'commits': 100, 'deletions': -500, 'name': 'Peter Shor'},
+            {'additions': 500, 'commits': 100, 'deletions': -500, 'name': 'Peter Shor'},
+            {'additions': 500, 'commits': 100, 'deletions': -500, 'name': 'Peter Shor'},
+            {'additions': 500, 'commits': 100, 'deletions': -500, 'name': 'Peter Shor'},
+            {'additions': 500, 'commits': 100, 'deletions': -500, 'name': 'Peter Shor'}]
     assert MetaCollector('TestRepoOwner', 'TestRepoName').get_contributors() == contributor_list
+
+@patch('monkeys.MonkeyStatsCodeFrequency')
+@patch('monkeys.MonkeyRepo')
+@patch('github.Github', mock.MagicMock(return_value=monkeys.MonkeyGithub()))
+def test_get_total_adds_and_dels(repo_mock, stats_mock):
+    """
+    Getting the total number of additions and deletions.
+    """
+    repo_mock.return_value.get_stats_code_frequency.return_value = [monkeys.MonkeyStatsCodeFrequency() for i in range(10)]
+    stats_mock.return_value.additions = 10000
+    stats_mock.return_value.deletions = -10000
+    assert MetaCollector('TestRepoOwner', 'TestRepoName').get_total_adds_and_dels() == {'additions': 100000, 'deletions': -100000}
+
+    repo_mock.return_value.get_stats_code_frequency.return_value = [monkeys.MonkeyStatsCodeFrequency() for i in range(100)]
+    stats_mock.return_value.additions = 20
+    stats_mock.return_value.deletions = -200
+    assert MetaCollector('TestRepoOwner', 'TestRepoName').get_total_adds_and_dels() == {'additions': 2000, 'deletions': -20000}
+
+@patch('monkeys.MonkeyWeeks')
+@patch('monkeys.MonkeyStatsCodeFrequency')
+@patch('monkeys.MonkeyRepo')
+@patch('github.Github', mock.MagicMock(return_value=monkeys.MonkeyGithub()))
+def test_core_developers(repo_mock, stats_mock, weeks_mock):
+    """
+    Getting a list of core developers.
+    """
+
+    repo_mock.return_value.get_stats_contributors.return_value = [monkeys.MonkeyStatsContributor() for i in range(5)]
+    stats_mock.return_value.weeks = [monkeys.MonkeyWeeks() for i in range(5)]
+    stats_mock.return_value.author= monkeys.MonkeyUser()
+
+    repo_mock.return_value.get_stats_code_frequency.return_value = [monkeys.MonkeyStatsCodeFrequency() for i in range(100)]
+    stats_mock.return_value.additions = 10
+    stats_mock.return_value.deletions = -10
+
+    weeks_mock.return_value.a = 100
+    weeks_mock.return_value.d = -100
+    weeks_mock.return_value.c = 20
+
+    assert MetaCollector('TestRepoOwner', 'TestRepoName').core_developers == [{'name': 'Peter Shor', 'additions': 500, 'deletions': -500, 'commits': 100}, {'name': 'Peter Shor', 'additions': 500, 'deletions': -500, 'commits': 100}, {'name': 'Peter Shor', 'additions': 500, 'deletions': -500, 'commits': 100}, {'name': 'Peter Shor', 'additions': 500, 'deletions': -500, 'commits': 100}, {'name': 'Peter Shor', 'additions': 500, 'deletions': -500, 'commits': 100}] 
+
+    repo_mock.return_value.get_stats_contributors.return_value = [monkeys.MonkeyStatsContributor() for i in range(5)]
+    repo_mock.return_value.get_commits.return_value = list(range(10))
+    stats_mock.return_value.weeks = [monkeys.MonkeyWeeks() for i in range(5)]
+    stats_mock.return_value.author= monkeys.MonkeyUser()
+
+    repo_mock.return_value.get_stats_code_frequency.return_value = [monkeys.MonkeyStatsCodeFrequency() for i in range(100)]
+    stats_mock.return_value.additions = 10
+    stats_mock.return_value.deletions = -10
+
+    weeks_mock.return_value.a = 1
+    weeks_mock.return_value.d = -1
+    weeks_mock.return_value.c = 0
+
+    assert len(MetaCollector('TestRepoOwner', 'TestRepoName').core_developers) == 0
